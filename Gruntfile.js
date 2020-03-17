6e85457109dcf242ddfd0a19e9b27fcd84d0dc88
ziyeqingying <1220167224@qq.com>
2020-03-17 16:31:19 +0800

    初始化

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+
+        htmlmin: {
+            options: {
+                collapseWhitespace: true
+            },
+            files: {
+                src: "dist/index.html",
+                dest: "dist/index.html"
+            }
+        },
+        copy: {
+            html: {
+                src: "./index.html",
+                dest: "./dist/index.html"
+            }
+        }
+    })
+
+
+    grunt.loadNpmTasks("grunt-contrib-htmlmin");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+
+    grunt.registerTask("release", ['copy', 'htmlmin']);
+}
