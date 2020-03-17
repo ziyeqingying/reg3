e4ab8ee6e35cc9473fac68f3c5082111f0fcf0b6
ziyeqingying <1220167224@qq.com>
2020-03-17 16:50:46 +0800

    66666

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,25 +15,12 @@ module.exports = function(grunt) {
                 src: "./index.html", 
                 dest: "./dist/index.html"
             }
-        },
-        cssmin: {
-            "dist/index.css": "index.css"
-        },
-        uglify: {
-            release: {
-                files: {
-                    "index.js": "dist/index.js"
-                }
-            }
         }
     })
 
 
     grunt.loadNpmTasks("grunt-contrib-htmlmin");
     grunt.loadNpmTasks("grunt-contrib-copy");
-    grunt.loadNpmTasks("grunt-contrib-htmlmin");
-    grunt.loadNpmTasks("grunt-contrib-cssmin");
-    grunt.loadNpmTasks("grunt-contrib-uglify");
 
-    grunt.registerTask("release", ['copy', 'htmlmin', "cssmin", "uglify"]);
+    grunt.registerTask("release", ['copy', 'htmlmin']);
 }
